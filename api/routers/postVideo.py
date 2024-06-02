2af867fc16e3f5b18d83b072d568ae345fd261c8
jerry <lwj0173@gmail.com>
2024-06-02 21:40:44 +0900

    ffmpeg webm->mp

--- api/routers/postVideo.py
+++ api/routers/postVideo.py
@@ -1,6 +1,6 @@
-
 import cv2
 import os
+import subprocess
 from fastapi import File, HTTPException, UploadFile, APIRouter
 import numpy as np
 import pandas as pd
@@ -15,47 +15,62 @@ from .model.stt.saveTempFile import save_temp_file
 from .model.imageModel import faceDetection, faceExtraction, imageModel
 
 
-router=APIRouter()
+router = APIRouter()
+
+async def convert_webm_to_mp3(input_path: str, output_path: str):
+    command = ['ffmpeg', '-i', input_path, '-q:a', '0', '-map', 'a', output_path]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500, detail=f"Error converting to mp3: {e}")
+
+async def convert_webm_to_mp4(input_path: str, output_path: str):
+    command = ['ffmpeg', '-i', input_path, '-c:v', 'libx264', '-c:a', 'aac', output_path]
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=500, detail=f"Error converting to mp4: {e}")
 
 @router.post("/upload-video/{roomID}/{userID}/{count}")
-async def upload_video(roomID: float, userID : float, count : int, audiofile: UploadFile = File(...),videofile: UploadFile = File(...)):
-    #백엔드 url
+async def upload_video(roomID: float, userID: float, count: int, audiofile: UploadFile = File(...), videofile: UploadFile = File(...)):
     base_url = "http://43.203.209.38:8080"
-    #stt
+    
     temp_audio_file_path = await save_temp_file(audiofile)
     temp_video_file_path = await save_temp_file(videofile)
+    
+    temp_mp3_path = temp_audio_file_path.replace(".webm", ".mp3")
+    temp_mp4_path = temp_video_file_path.replace(".webm", ".mp4")
 
+    await convert_webm_to_mp3(temp_audio_file_path, temp_mp3_path)
+    await convert_webm_to_mp4(temp_video_file_path, temp_mp4_path)
+    
     try:
-        if temp_audio_file_path and os.path.exists(temp_audio_file_path):  # 파일 경로 확인
-            sr = get_sample_rate(temp_audio_file_path)  # 파일 샘플 레이트 추출
-            df = stt(temp_audio_file_path)  # 음성 인식
+        if temp_mp3_path and os.path.exists(temp_mp3_path):  # 파일 경로 확인
+            sr = get_sample_rate(temp_mp3_path)  # 파일 샘플 레이트 추출
+            df = stt(temp_mp3_path)  # 음성 인식
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")  # 로그에 오류 메시지 출력
-        if temp_audio_file_path and os.path.exists(temp_audio_file_path):
-            os.remove(temp_audio_file_path)
+        if temp_mp3_path and os.path.exists(temp_mp3_path):
+            os.remove(temp_mp3_path)
         raise HTTPException(status_code=500, detail=str(e))
     
-    #STT POST
-
     for i in range(len(df)):
         content = df['text'].iloc[i]
-        millisec = df['start'].iloc[i]+10000*count
-        stt_result=stt_Result.stt_Result(content=content,millisec=millisec)
-        save_stt_url=f'{base_url}/live/{int(roomID)}/{int(userID)}/save/content'
+        millisec = df['start'].iloc[i] + 10000 * count
+        stt_result = stt_Result.stt_Result(content=content, millisec=millisec)
+        save_stt_url = f'{base_url}/live/{int(roomID)}/{int(userID)}/save/content'
         try:
             response = requests.post(save_stt_url, json=stt_result.dict())
             response.raise_for_status()  # 상태 코드가 200이 아니면 예외 발생
         except requests.RequestException as e:
             raise HTTPException(status_code=500, detail=str(e))
-        
-
-    #음성 모델
+    
     X_audio = audio_cut(df)
-    dfText=pd.DataFrame(df['text'])
-    print(X_audio.shape,dfText.shape)
+    dfText = pd.DataFrame(df['text'])
+    print(X_audio.shape, dfText.shape)
     X = pd.concat([X_audio, dfText], axis=1)
-    txt_embed = text_embedding(model_name = 'jhgan/ko-sbert-multitask')
+    txt_embed = text_embedding(model_name='jhgan/ko-sbert-multitask')
     scaler = StandardScaler()
     X = txt_embed.transform(X)
     X = scaler.fit_transform(X)
@@ -66,25 +81,24 @@ async def upload_video(roomID: float, userID : float, count : int, audiofile: Up
 
     model.load_weights(weights_path)
     prediction = model.predict(X)
-    prediction=pd.DataFrame(prediction)
+    prediction = pd.DataFrame(prediction)
     
     s = []
     for i in range(len(prediction)):
         score = (prediction.iloc[i, 0] * 1 +
-             prediction.iloc[i, 1] * 1 +
-             prediction.iloc[i, 2] * 1 +
-             prediction.iloc[i, 3] * 8 +
-             prediction.iloc[i, 4] * 5 +
-             prediction.iloc[i, 5] * 1)
+                 prediction.iloc[i, 1] * 1 +
+                 prediction.iloc[i, 2] * 1 +
+                 prediction.iloc[i, 3] * 8 +
+                 prediction.iloc[i, 4] * 5 +
+                 prediction.iloc[i, 5] * 1)
         score = score * 10
         s.append(score)
     audio_final_score = (float)(round(sum(s) / len(s), 2))
     
-    #이미지 모델
     img_final_score_list = []
     score = []
 
-    cap = cv2.VideoCapture(temp_video_file_path)
+    cap = cv2.VideoCapture(temp_mp4_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     interval = 0.2
     frame_interval = int(fps * interval)
@@ -101,52 +115,47 @@ async def upload_video(roomID: float, userID : float, count : int, audiofile: Up
                 face_img = np.reshape(face_zoom[0], (48, 48, 1))
                 face_img_batch = np.expand_dims(face_img, axis=0)
 
-                imgModel=imageModel.img_model()
+                imgModel = imageModel.img_model()
                 try:
                     predictions = np.array(imgModel.predict(face_img_batch)[0])
                 except Exception as e:
                     print(f"Error during prediction: {str(e)}")
                     raise HTTPException(status_code=500, detail="Prediction error")
 
-                #s는 1개의 장면에서의 점수
-                s = (predictions[0]*0 + predictions[1]*5 + predictions[2]*10) * 10
+                s = (predictions[0] * 0 + predictions[1] * 5 + predictions[2] * 10) * 10
                 score.append(s)
                 c += 1
                 print(f'counts : {c}')
                 
-        # 다음 프레임 읽기
         success, image = cap.read()
-        cnt += 1  # 프레임 카운트 증가
+        cnt += 1
 
     if len(score) > 0:
         d = round(sum(score) / len(score), 2)
     else:
-        d = 0  # score 리스트가 비어있는 경우
+        d = 0
     img_final_score_list.append(d)
-    img_final_score=img_final_score_list[0]
+    img_final_score = img_final_score_list[0]
 
-    #10초단위 종합점수 POST
-    save_emotion_url=f'{base_url}/live/{int(roomID)}/{int(userID)}/save/emotion'
+    save_emotion_url = f'{base_url}/live/{int(roomID)}/{int(userID)}/save/emotion'
     try:
         audio_final_score = int(round(audio_final_score))
-        img_final_score=int(round(img_final_score))
-        emotion_result=emotion_Result.emotion_Result(image=img_final_score,voice=audio_final_score)
+        img_final_score = int(round(img_final_score))
+        emotion_result = emotion_Result.emotion_Result(image=img_final_score, voice=audio_final_score)
         response = requests.post(save_emotion_url, json=emotion_result.dict())
         response.raise_for_status()  # 상태 코드가 200이 아니면 예외 발생
     except requests.RequestException as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-    #초기화
-    if temp_audio_file_path and os.path.exists(temp_audio_file_path):
-            os.remove(temp_audio_file_path)
-            print(f"Deleted temp audio file: {temp_audio_file_path}")
+    if temp_mp3_path and os.path.exists(temp_mp3_path):
+        os.remove(temp_mp3_path)
+        print(f"Deleted temp mp3 file: {temp_mp3_path}")
             
-    if temp_video_file_path and os.path.exists(temp_video_file_path):
-            os.remove(temp_video_file_path)
-            print(f"Deleted temp video file: {temp_video_file_path}")
+    if temp_mp4_path and os.path.exists(temp_mp4_path):
+        os.remove(temp_mp4_path)
+        print(f"Deleted temp mp4 file: {temp_mp4_path}")
 
-    score.clear()  # 다음 파일을 위해 score 리스트 초기화
+    score.clear()
     cap.release()
 
     return
-    
